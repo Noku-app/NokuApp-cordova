0f3a520de4bda9575406f3fe1ec342b6fd9cd05e
Tobi <=>
2021-01-25 03:17:37 -0600

    fileio reformatted

--- www/js/fileio.js
+++ www/js/fileio.js
@@ -1,48 +1,70 @@
-function getInternalStorage(){
+const getInternalStorage = () => {
     return window.resolveLocalFileSystemURL(cordova.file.applicationDirectory);
 }
 
-function getFileHandle(id, path, handler, error){
-    if(error == null) error = onIOError();
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
-
-        console.log('file system open: ' + fs.name);
-        fs.root.getFile(path, { create: true, exclusive: false }, function (fileEntry) {
-            handler(id, fileEntry);
-        }, error(id));
-    }, error(id));
+const getFileHandle = (id, path, handler, error) => {
+        if(!error) {
+            error = onIOError(id);
+        };
+        window.requestFileSystem(
+            LocalFileSystem.PERSISTENT, 
+            0, 
+            fs => {
+                console.log(`file system open: ${fs.name}`);
+                fs.root.getFile(
+                    path, 
+                    { create: true, exclusive: false }, 
+                    fileEntry => {
+                        handler(id, fileEntry);
+                    }, 
+                    error(id)
+                );
+        }, error(id)
+    );
 }
 
-function writeToFile(id, fileEntry, dataObj, success, error){
-    fileEntry.createWriter(function (fileWriter) {
-        fileWriter.onwriteend = function(e) {
-            if(success != null) success();
-            console.log("Successfully wrote to: " + e.toString());
-        };
+const writeToFile = (id, fileEntry, dataObj, success, error) => {
+    fileEntry.createWriter(
+        fileWriter => {
 
-        fileWriter.onerror = function (e) {
-            if(error != null) error();
-            else(onIOError(id));
-            console.log("Failed file write: " + e.toString());
-        };
+            fileWriter.onwriteend = err => {
+                if(success) {
+                    success();
+                }; console.log(`Successfully wrote to: ${err.toString()}`);
+            };
 
-        if(dataObj != null) fileWriter.write(dataObj);
-    });
+            fileWriter.onerror = err => {
+                if(err) {
+                    error()
+                } else {
+                    onIOError(id)
+                }; console.log(`Failed file write: ${err.toString()}`);
+            };
+
+            if(!dataObj) {
+                fileWriter.write(dataObj);
+            }
+        }
+    );
+};
+
+const readFile = (id, fileEntry, success, error) => {
+    if(!error) {
+        error = onIOError(id);
+    };
+    fileEntry.file(
+        file => {
+            var reader = new FileReader();
+            reader.onloadend = () => {
+                alert(this.result);
+                success(id, this.result);
+            };
+            reader.readAsText(file);
+        }, 
+        error(id)
+    );
 }
 
-function readFile(id, fileEntry, success, error) {
-    if(error == null) error = onIOError(id)
-    fileEntry.file(function (file) {
-        var reader = new FileReader();
-
-        reader.onloadend = function() {
-            alert(this.result);
-            success(id, this.result);
-        };
-        reader.readAsText(file);
-    }, error(id));
-}
-
-function onIOError(id){
+const onIOError = id => {
 
 }
